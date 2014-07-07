475b7e57496f6876f7d15f7002c9c5700134132b
Your Name <you@example.com>
2014-07-07 09:04:00 +0300

    init

---
+++ treetoxml/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'treetoxml.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('treetoxml.src.main.urls')),
+    url(r'^main/', include('treetoxml.src.main.urls')),
+)
